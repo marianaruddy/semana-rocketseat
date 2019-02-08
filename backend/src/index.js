6836a735985c33091bfa9e744ffdd7685e7375c2
marianaruddy <marianamrsantos@gmail.com>
2019-02-08 11:59:54 -0200

    cria backend

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,11 +1,28 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
 
 const app = express();
 
-app.get('/', (req, res) => {
-  return res.send('Hello World');
+const server = require('http').Server(app);
+const io = require("socket.io")(server);
+
+mongoose.connect('mongodb://goweek:Anairam7@ds123625.mlab.com:23625/goweek-backend-mariana', {
+  useNewUrlParser: true
 });
 
-app.listen(3000, () => {
-  console.log('server started on port 3000');
+app.use((req, res, next) => {
+  req.io = io;
+
+  return next();
+});
+
+app.use(cors());
+
+app.use(express.json());
+
+app.use(require('./routes'));
+
+server.listen(3001, () => {
+  console.log('server started on port 3001');
 });
