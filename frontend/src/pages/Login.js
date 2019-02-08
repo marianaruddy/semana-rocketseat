690bbf052654a609fad94942b7ee789c87f85100
marianaruddy <marianamrsantos@gmail.com>
2019-02-08 15:24:50 -0200

    cria react app

---
+++ frontend/src/pages/Login.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+import twitterLogo from '../twitter.svg' 
+import './Login.css';
+
+export default class Login extends Component {
+  render() {
+    return <h1>Hello world</h1>;
+  }
+}
